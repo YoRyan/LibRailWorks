5d029f533bff01ba6a5cc09780d4c6b444090381
Ryan Young <ryan@youngryan.com>
2022-04-13 22:05:31 -0700

    fix: totally prevent sparks from showing when stopped, and recalibrate speeds
    Based on advice from CTSL Railfan that sparks generally show up at speeds above 100 mph.

--- RollingStock/Spark.lua
+++ RollingStock/Spark.lua
@@ -9,12 +9,8 @@ function P:new(conf)
   local o = {
     _tick_s = conf.tick_s or 0.2,
     _getmeantimebetween_s = conf.getmeantimebetween_s or function(aspeed_mps)
-      -- Calibrated for 22 mph = 30 s, with a rapid falloff thereafter.
-      if aspeed_mps == 0 then
-        return 60 * 60 * 24 * 365
-      else
-        return 300 / aspeed_mps
-      end
+      -- Calibrated for 100 mph = 30 s, with a rapid falloff for lower speeds.
+      return 1341 / aspeed_mps
     end,
     _lasttick_s = nil,
     _showspark = false
@@ -29,8 +25,12 @@ function P:isspark()
   local now = RailWorks.GetSimulationTime()
   if self._lasttick_s == nil or now - self._lasttick_s > self._tick_s then
     local aspeed_mps = math.abs(RailWorks.GetSpeed())
-    local meantime = self._getmeantimebetween_s(aspeed_mps)
-    self._showspark = math.random() < self._tick_s / meantime
+    if aspeed_mps > Misc.stopped_mps then
+      local meantime = self._getmeantimebetween_s(aspeed_mps)
+      self._showspark = math.random() < self._tick_s / meantime
+    else
+      self._showspark = false
+    end
     self._lasttick_s = now
   end
   return self._showspark
